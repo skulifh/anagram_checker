6db50c4d0f53d0589c18c35edc2a9cf64c47ad88
Skúli Freyr Hinriksson <skuli@sareye.com>
2016-11-30 23:30:03 +0100

    did a bit of code cleanup and commented

--- anagram.py
+++ anagram.py
@@ -1,14 +1,36 @@
-#Could have done:
-#Removing lines that have been read.
-#Make it multi threaded. Maybe divide the file to the threads
+#Notes:
+#This program first eliminates all the word from the given word file to minimize the work needed. It does this by removing
+#any words from the wordfile that contains characters that are not a part of the given hint phrase. It also elimintaes all 
+#single character words such as 'a', 'b', or 'c's' but leaves in 'i' and 'a'. It also eliminates all duplications.
+
+#When the program finds an anagram that contain excatly as many letters as the given hint phrase (18 character long) it checks
+#the corresponding hash and logs the word and the hash into a file. If the hash is the same as the given hint hash then the program
+#shuts down since it has completed it's task.
+
+#The program does not use very little RAM memory but instead uses the hard disk space since that is just as quick in this case,
+#and using RAM would be very bad since we have to work with huge amount of data later on.
+
+#The program only uses a single thread. It would be possible to make this run on many concurrent threads since it the nature
+#of this data analysis is not dependent on anything that it does previously.
+
+#The program took around 45 minutes to find the secret phrase on a single CPU. It takes around 24 hours to go through
+#the whole word file to find all possible anagrams of the given hint phrase. 
+
+#-----------
+
+#How to run:
+#python anagram.py 'poultry outwits ants'
+
+#-----------
+
+#Requirements:
+#Python 2.7.10
 
 import sys
-import time
-import pdb
 import hashlib
 
 hint = sys.argv[1]
-hint_length = len(hint.strip().replace(' ', '').replace('\'', '')) #todo: Automate this
+hint_length = len(hint.strip().replace(' ', '').replace('\'', ''))
 potential_words = []
 log_file = open('log_file', 'w')
 potential_phrases2_file = open('potential_phrases2_file', 'w')
@@ -35,7 +57,7 @@ def included_check(word):
 	return consistent
 
 
-#Find all the words that have characters that fit in the hint phrase
+#Find all the words from the word file that have characters that fit in the hint phrase
 with open(word_file) as infile:
 	for word in infile:
 		
@@ -47,7 +69,7 @@ with open(word_file) as infile:
 
 potential_phrases2_file_second = 'potential_phrases2_file'
 
-#todo: Combine this one with the previous one
+#Going through the words found and combining to see what phases fit into the given hint
 with open(potential_phrases2_file_second, 'rw') as inline:
 	for phrase in inline:
 
@@ -58,14 +80,17 @@ with open(potential_phrases2_file_second, 'rw') as inline:
 			if (len(combined_word) <= 18) and (included_check(combined_word)):
 
 				if (len(combined_word) == 18):
-					#Possible to add a checker here to kill the program once the correct hash has been found.
+					#Adding all phrases that are complete anagrams of the given phrase into a file
 					log_file.write(copy + '\n')
 					log_file.write(hashlib.md5(copy).hexdigest() + '\n\n')
+
+					#If we find the given hash we kill the program since we have found the phrase we are looking for
 					if (hashlib.md5(copy).hexdigest() == target_hash):
 						kill = True
 						break
 
 				else:
+					#Adding the phrases that fit into the given hint phrase into a file to be read and checked later
 					potential_phrases2_file.write(copy + '\n')
 		if kill:
 			break
